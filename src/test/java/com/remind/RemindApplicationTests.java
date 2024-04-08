ea2214eef8465434b29f19ddf3632f843ccfd584
Lee sang min <dkwkghkdlxld2@gmail.com>
2024-04-08 19:12:20 +0900

    project init

---
+++ src/test/java/com/remind/RemindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
