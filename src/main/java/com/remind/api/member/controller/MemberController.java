a1cc3099e7854c87bf7ae898dc76eb48c0fb9f79
Lee sang min <dkwkghkdlxld2@gmail.com>
2024-04-14 16:38:24 +0900

    docs: swagger 설정 + 토큰 갱신 API 문서화

--- src/main/java/com/remind/api/member/controller/MemberController.java
+++ src/main/java/com/remind/api/member/controller/MemberController.java
@@ -1,9 +1,12 @@
 package com.remind.api.member.controller;
 
 import com.remind.api.member.dto.request.RefreshTokenRequestDto;
+import com.remind.api.member.dto.response.TokenResponseDto;
 import com.remind.api.member.service.MemberService;
 import com.remind.core.domain.common.response.ApiSuccessResponse;
 import com.remind.core.security.dto.UserDetailsImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,19 +19,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/member")
 @RequiredArgsConstructor
+@Tag(name = "member API", description = "member 관련 API 문서")
 public class MemberController {
 
     private final MemberService memberService;
 
     //>>>>  소셜 로그인 구현 예정
     @PostMapping("/login")
-    public void login(){
+    public String login() {
+        return "login 성공";
 
     }
 
-
+    @Operation(
+            summary = "토큰 갱신 API",
+            description = "refresh token을 사용하여 access token와 refresh token을 갱신한다."
+    )
     @PostMapping("/refresh")
-    public ResponseEntity<ApiSuccessResponse<?>> refreshToken(
+    public ResponseEntity<ApiSuccessResponse<TokenResponseDto>> refreshToken(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
             @Valid @RequestBody RefreshTokenRequestDto dto
     ) {
---
+++ src/main/java/com/remind/core/infra/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.remind.core.infra.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.In;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    private final String SCHEME_NAME = "Authorization";
+    private final String BEARER_FORMAT = "JWT";
+    private final String SCHEME = "Bearer";
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(
+                                SCHEME_NAME, new SecurityScheme()
+                                        .name(SCHEME_NAME)
+                                        .type(Type.HTTP)
+                                        .bearerFormat(BEARER_FORMAT)
+                                        .in(In.HEADER)
+                                        .scheme(SCHEME)))
+                .info(new Info()
+                        .title("ReMind 프로젝트 API 문서")
+                        .description("큐시즘 밋업 ReMind 프로젝트 API 문서")
+                        .version("1.0.0"));
+    }
+}
