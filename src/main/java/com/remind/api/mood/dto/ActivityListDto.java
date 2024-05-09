1d68fdaa2bf594c0e1a36a00ccbc815bda6e596c
leesangmin <dkwkghkdlxld2@gmail.com>
2024-05-10 00:23:48 +0900

    feat(#12): model 반환 데이터 수정

--- src/main/java/com/remind/api/mood/dto/ActivityListDto.java
+++ src/main/java/com/remind/api/mood/dto/ActivityListDto.java
@@ -6,13 +6,15 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "추가한 활동 리스트 model")
 public record ActivityListDto(
+        @Schema(description = "활동 ID")
+        Long activityId,
         @Schema(description = "활동명")
         String name,
         @Schema(description = "아이콘 이미지")
         String iconImage
 ) {
 
-    public static ActivityListDto fromEntity(Activity activity){
-        return new ActivityListDto(activity.getActivityName(),activity.getActivityIcon());
+    public static ActivityListDto fromEntity(Activity activity) {
+        return new ActivityListDto(activity.getId(), activity.getActivityName(), activity.getActivityIcon());
     }
 }
