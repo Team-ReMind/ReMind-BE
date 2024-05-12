65e5e13629085fc95ed11f6c96ebc2fa8b679af6
Jinwoo Park <wlsdndml213@naver.com>
2024-05-12 18:19:54 +0900

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,7 @@
 use
 remind;
 
-insert into member(id, auth_id, name, age, gender, email, phone_number, profile_image_url, is_onboarding_finished,
-                   registration_token, roles_type)
-VALUES (1, 1, '이상민', 27, 'men', '1234@gmail.com', '010-0000-0000', 'test-image-url', true, 'test-token', 'ROLE_USER');
+INSERT INTO member(id, auth_id, name, age, gender, email, phone_number, profile_image_url, is_onboarding_finished,
+                   registration_token, roles_type, protector_phone_number, city, district, center_name)
+VALUES (1, 1, '이상민', 27, 'men', '1234@gmail.com', '010-0000-0000', 'test-image-url', true, 'test-token', 'ROLE_USER', 
+        '010-1111-2222', 'Seoul', 'Gangnam', 'Center A');
