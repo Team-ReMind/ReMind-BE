f5056ca9a1c9915bf5265f01d676859c2c257642
Lee sang min <dkwkghkdlxld2@gmail.com>
2024-05-19 13:21:45 +0900

    feature(#55): 고정 활동 비즈니스 로직 추가

--- src/main/java/com/remind/core/domain/mood/repository/FixActivityRepository.java
+++ src/main/java/com/remind/core/domain/mood/repository/FixActivityRepository.java
@@ -1,12 +1,8 @@
 package com.remind.core.domain.mood.repository;
 
 import com.remind.core.domain.mood.FixActivity;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface FixActivityRepository extends JpaRepository<FixActivity, Long> {
 
-    @Query("select fixActivity from FixActivity fixActivity")
-    List<FixActivity> findAllCustom();
 }
