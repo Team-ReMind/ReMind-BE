70c63df900fa4423f5b27658bc5dab5d12a5f550
j2noo <wlsdndml213@gmail.com>
2024-05-20 15:39:48 +0900

    fix(#59) : 스웨거 @Operation 개행 수정

--- src/main/java/com/remind/api/takingMedicine/controller/TakingMedicineController.java
+++ src/main/java/com/remind/api/takingMedicine/controller/TakingMedicineController.java
@@ -28,8 +28,8 @@ import java.time.LocalDate;
 public class TakingMedicineController {
     private final TakingMedicineService takingMedicineService;
     @Operation(
-            summary = "memberId(), 날짜로 해당 날짜의 약 복용 정보를 조회하는 api",
-            description = "나의 정보를 조회하는 경우에는 memberId = 0 을 넣어주세요." +
+            summary = "memberId(), 날짜로 해당 날짜의 약 복용 정보를 조회하는 api\n",
+            description = "나의 정보를 조회하는 경우에는 memberId = 0 을 넣어주세요.\n" +
                     "date는 string으로, YYYY-MM-DD 형식으로 주세요."
     )
     @GetMapping("/daily")
@@ -42,8 +42,12 @@ public class TakingMedicineController {
     }
 
     @Operation(
-            summary = "memberId, year, month로 월 단위 약 복용 정보를 조회하는 api",
-            description = "memberId, year, month로 월 단위 약 복용 정보를 조회하는 api\n 나의 정보를 조회하는 경우에는 memberId = 0 을 넣어주세요\""
+            summary = "memberId, year, month로 월 단위 약 복용 정보를 조회하는 api <br> g",
+            description = "해당 월 1일부터 말일까지의 데이터가 모두 들어있습니다. <br> g" +
+                    " needMedicine : false인 경우, 약을 복용하지 않아도 되는 날 <br> g" +
+                    " 나의 정보를 조회하는 경우에는 memberId = 0 을 넣어주세요 <br> g" +
+                    "taking level : 약 복용 정도(0 : 미복용, 1 : 부분 복용, 2 : 모두 복용 완료 <br> g" +
+                    "taking count :  그 날 약 복용 횟수, 단순 참고용"
     )
     @GetMapping("/monthly")
     public ResponseEntity<ApiSuccessResponse<MonthlyTakingMedicineInfoResponse>> getMonthlyTakingMedicineInfo(
@@ -58,7 +62,7 @@ public class TakingMedicineController {
 
     @Operation(
             summary = "특정 날짜의 약 복용 정보를 등록하는 api",
-            description = "medicineType : BREAKFAST, LUNCH, DINNER, ETC" +
+            description = "medicineType : BREAKFAST, LUNCH, DINNER, ETC <br> " +
                     "약을 복용하지 않은 경우 notTakingReason을 비워두거나 빈 문자열"
     )
     @PostMapping("")
@@ -72,7 +76,7 @@ public class TakingMedicineController {
 
     @Operation(
             summary = "특정 멤버의 약 복용률을 반환하는 api",
-            description = "특정 날짜의 약 복용률 조회 api\n 나의 정보를 조회하는 경우에는 memberId = 0 을 넣어주세요\""
+            description = "특정 날짜의 약 복용률 조회 api <br> 나의 정보를 조회하는 경우에는 memberId = 0 을 넣어주세요\""
     )
     @GetMapping("/rate")
     public ResponseEntity<ApiSuccessResponse<TakingMedicineRateResponse>> getTakingMedicineRate(
