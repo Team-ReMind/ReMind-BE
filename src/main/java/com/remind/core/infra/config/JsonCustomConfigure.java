bd1b38a2c2b397203e08615b5b034e5441047bca
j2noo <wlsdndml213@gmail.com>
2024-05-22 02:04:54 +0900

    fix(#70) : LocalTime -> JSON 매핑 오류 수정

--- src/main/java/com/remind/core/infra/config/JsonCustomConfigure.java
+++ src/main/java/com/remind/core/infra/config/JsonCustomConfigure.java
@@ -1,16 +1,17 @@
 package com.remind.core.infra.config;
 
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class JsonCustomConfigure {
@@ -25,7 +26,15 @@ public class JsonCustomConfigure {
     static class CustomObjectMapper extends ObjectMapper {
         private static final long serialVersionUID = 1L;
         public CustomObjectMapper() {
+            //localTIme OBJect를 매핑하는 코드 추가
+            JavaTimeModule javaTimeModule = new JavaTimeModule();
+            javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ISO_LOCAL_DATE));
+            javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ISO_LOCAL_DATE));
+            registerModule(javaTimeModule);
+
             getSerializerProvider().setNullValueSerializer(new NullToEmptyStringSerializer());
+
+            configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         }
     }
         @Bean
